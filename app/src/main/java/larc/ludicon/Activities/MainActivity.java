01866853b61863d905afa2a0076510dc89352507
Andrei <andreiursache94@gmail.com>
2015-12-04 20:14:32 +0200

    Parse Connection
    It has a little bug (hope we'll resolve now).

--- app/src/main/java/larc/ludicon/Activities/MainActivity.java
+++ app/src/main/java/larc/ludicon/Activities/MainActivity.java
@@ -12,6 +12,10 @@ import android.widget.Button;
 
 import com.facebook.login.LoginManager;
 import com.facebook.login.widget.LoginButton;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import larc.ludicon.R;
 import larc.ludicon.SharedPreferences.UserIdSave;
@@ -24,12 +28,41 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button popupButton = (Button)findViewById(R.id.PopUpbutton);
+
+          /* Initialize Parse connection */
+        Parse.initialize(this, "7ynxx7uuHFrR4b5tEDDv3yEOPIFhcjsdSIUfDGxh", "sVYUFfdDYLmuqhxU9pxSVvdxRioC3jurlNJb41cw");
+
         popupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,Popup.class));
             }
         });
+
+//        TODO - Here - Andrei and Ciprian merge the code for users
+//        This is an example (add a user to cloud) - we should do the same:
+
+        ParseUser user = new ParseUser();
+        user.setUsername("bulaDePeMarte");
+        user.setPassword("MartianaMea");
+        user.setEmail("martianuxxx@marte.mr");
+
+// other fields can be set just like with ParseObject
+        user.put("phone", "650-555-0000");
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                }
+            }
+        });
+
+
+
         Button fakeLogin = (Button)findViewById(R.id.fakeLoginButton);
         fakeLogin.setOnClickListener(new View.OnClickListener() {
             @Override
