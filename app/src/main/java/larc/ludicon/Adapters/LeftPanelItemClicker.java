6b3c71c982e83708ad050feb407acf5bd009e4c1
Neagu, Laurentiu <Laurentiu.Neagu@fisglobal.com>
2016-08-11 00:46:42 +0300

    blocked rankings activity until better design is created.

--- app/src/main/java/larc/ludicon/Adapters/LeftPanelItemClicker.java
+++ app/src/main/java/larc/ludicon/Adapters/LeftPanelItemClicker.java
@@ -42,8 +42,9 @@ public class LeftPanelItemClicker {
                                 i_currActivity.startActivity(chatIntent);
                                 break;
                             case 4:
-                                Intent rankingIntent = new Intent(i_currActivity, RankingsActivity.class);
-                                i_currActivity.startActivity(rankingIntent);
+                                //Intent rankingIntent = new Intent(i_currActivity, RankingsActivity.class);
+                                //i_currActivity.startActivity(rankingIntent);
+                                Toast.makeText(i_context,"Will be available soon..",Toast.LENGTH_LONG).show();
                                 break;
                             case 5:
                                 Intent helpIntent = new Intent(i_currActivity, SettingsActivity.class);
